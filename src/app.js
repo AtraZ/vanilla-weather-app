ea83fc4e4cfd8f04c29b9b8c10dc1e62a3d698bf
AtraZ <atra.zavarbashi@gmail.com>
2023-07-05 21:12:29 +0430

    fixed apiUrl. changed unit to metric.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ function displayTemp(response) {
 
 let apiKey = "45c9off7ae4d90355519f98bt6891ad3";
 let query = "london";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayCity);
 axios.get(apiUrl).then(displayTemp);
