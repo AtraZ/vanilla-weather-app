b1140143e6032b5fc5e4e7c7b1412064a95d7054
AtraZ <atra.zavarbashi@gmail.com>
2023-07-05 21:09:35 +0430

    fetching city name based on input and changing temp based on that.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
+function displayCity(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+}
+
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "45c9off7ae4d90355519f98bt6891ad3";
+let query = "london";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}=metric`;
+
+axios.get(apiUrl).then(displayCity);
 axios.get(apiUrl).then(displayTemp);
