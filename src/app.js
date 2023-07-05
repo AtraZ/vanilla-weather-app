5ea63fb95fdb6e41e775e5f9a5ab06d09ef29144
AtraZ <atra.zavarbashi@gmail.com>
2023-07-05 21:24:17 +0430

    fixed log problem regarding temperature.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function displayCity(response) {
 
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
 let apiKey = "45c9off7ae4d90355519f98bt6891ad3";
