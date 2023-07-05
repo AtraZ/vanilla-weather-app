441b48deac404d1a330a64e7dc9c8b89379d09bf
AtraZ <atra.zavarbashi@gmail.com>
2023-07-05 20:46:21 +0430

    apiUrl edited.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
 let apiKey = "45c9off7ae4d90355519f98bt6891ad3";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}=metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}=metric`;
