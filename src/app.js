e276f71847aa9d6811c43b10cb6b3c96b6f3d7dd
AtraZ <atra.zavarbashi@gmail.com>
2023-07-05 21:01:28 +0430

    created a function called displayTemp and fetched temperature from api based on city name.on t

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let apiKey = "45c9off7ae4d90355519f98bt6891ad3";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}=metric`;
+axios.get(apiUrl).then(displayTemp);
